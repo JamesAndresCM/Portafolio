34dd2b50a88b39eafc93c0733a3be21c17d5f740
JamesACM <jaimecanalesmarin@gmail.com>
2017-11-11 16:02:48 -0300

    se implement el formulario para los nested attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,19 +1,21 @@
 class PortfoliosController < ApplicationController	
 
 	def index
-		@portfolio_items = Portfolio.all
+	  @portfolio_items = Portfolio.all
 	end
 
 	def angular
-		@angular_portfolio_items = Portfolio.angular
+	  @angular_portfolio_items = Portfolio.angular
 	end
 
 	def new
-		@portfolio_item = Portfolio.new
+	  @portfolio_item = Portfolio.new
+	  3.times { @portfolio_item.technologies.build }
 	end
 
 	def create
-	 @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+	 @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, 
+					 technologies_attributes: [:name]))
 
 		respond_to do |format|
 			if @portfolio_item.save
@@ -26,11 +28,11 @@ class PortfoliosController < ApplicationController
 	end
 
 	def edit
-		@portfolio_item = Portfolio.find(params[:id])
+	  @portfolio_item = Portfolio.find(params[:id])
 	end
 
 	def update
-		@portfolio_item = Portfolio.find(params[:id])
+	  @portfolio_item = Portfolio.find(params[:id])
 		
 		respond_to do |format|
 			if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
@@ -42,16 +44,16 @@ class PortfoliosController < ApplicationController
 	end
 
 	def show
-		@portfolio_item = Portfolio.find(params[:id])
+	  @portfolio_item = Portfolio.find(params[:id])
 	end
 
 	def destroy
-		@portfolio_item = Portfolio.find(params[:id])
+	  @portfolio_item = Portfolio.find(params[:id])
 		
-		@portfolio_item.destroy
-		respond_to do |format|
-			format.html { redirect_to portfolios_path, notice: 'Portfolio was removed'}
-		end
+	  @portfolio_item.destroy
+	     respond_to do |format|
+		format.html { redirect_to portfolios_path, notice: 'Portfolio was removed'}
+	     end
 	end
 	
 end
