c7bf935360dae4cd7d8bde03ef704602a3a7df2e
JamesAndresCM <jaimecanalesmarin@gmail.com>
2018-07-24 18:19:55 -0400

    add mount-cable

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :topics, only: [:index, :show]
+  resources :topics, only: %i[index show]
 
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  devise_for :users, path: '', path_names: { sign_in: 'login',
+                                             sign_out: 'logout',
+                                             sign_up: 'register' }
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
   end
@@ -12,11 +14,12 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
   get 'tech-news', to: 'pages#tech_news'
 
-  resources :blogs do 
+  resources :blogs do
     member do
-       get :toggle_status
+      get :toggle_status
     end
   end
 
+  mount ActionCable.server => '/cable'
   root to: 'pages#home'
 end
