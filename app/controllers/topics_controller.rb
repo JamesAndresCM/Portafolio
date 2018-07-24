20241a0c1f4b2714a3cd56023f169c478b1f41f3
JamesAndresCM <jaimecanalesmarin@gmail.com>
2018-07-24 12:23:57 -0400

    fix rubocop ofenses

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -9,7 +9,7 @@ class TopicsController < ApplicationController
   def show
     @topic = Topic.find(params[:id])
 
-    @blogs = if logged_in?(:site_admin)
+    @blogs = if logged_in?(:site_admin) then
                @topic.blogs.recent.page(params[:page]).per(5)
              else
                @topic.blogs.published.recent.page(params[:page]).per(5)
